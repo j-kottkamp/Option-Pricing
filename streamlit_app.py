496a9c4b84b327c98d16d3875be6f32df834ea38
j-kottkamp <149945462+j-kottkamp@users.noreply.github.com>
2025-06-27 22:10:58 +0200

    minor improvements

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import runpy
 # Set working directory to main
 os.chdir(os.path.dirname(__file__))
 
-runpy.run_path("scripts/open_gui.py", run_name="__main__")
+runpy.run_path("main/scripts/open_gui.py", run_name="__main__")
